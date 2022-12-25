e0c4aab5dff053c91f7f29fdc09b8b36696413ae
Archit Patel <architpatel2607@gmail.com>
2022-12-25 14:12:15 +0530

    Added 1st program

---
+++ _01_leap_year.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class _01_leap_year {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int year;
+        year = sc.nextInt();
+
+        if (year%4 == 0) {
+            if(year%100 != 0)
+                System.out.println("Leap year");
+            if (year%400 == 0)
+                System.out.println("Leap year");
+            else System.out.println("Not a leap year");
+        }
+        else System.out.println("Not a leap year");
+    }
+} 
